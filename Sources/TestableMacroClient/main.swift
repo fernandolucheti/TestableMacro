dcc83fc4596f418b6d9d118ec909671395c351e3
Fernando Lucheti <fernandolucheti@gmail.com>
2024-10-08 14:48:58 -0300

    feat: create testable macro

---
+++ Sources/TestableMacroClient/main.swift
@@ -0,0 +1,16 @@
+import TestableMacro
+
+@Testable struct MyClass {
+    private var someVariable: Int = 0
+
+    private func someFunction() {
+    }
+    private func someFunction(param1: String, param2: Int) {
+        print("Hello, \(param1) with number \(param2)!")
+    }
+    private func anotherFunction2(_ param: Double, param2: Int) {
+        print("Another function with param: \(param)")
+    }
+    fileprivate func anotherFunction(in param: Double) {
+    }
+}
