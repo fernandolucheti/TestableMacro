fe3228b6b934c6c7642022778e69abaa59684bc3
Fernando Lucheti <fernandolucheti@gmail.com>
2024-10-22 18:37:13 -0300

    tests: added tests for @Testable macro

--- Tests/SwiftMacrosTests/SwiftMacrosTests.swift
+++ Tests/SwiftMacrosTests/SwiftMacrosTests.swift
@@ -3,46 +3,105 @@ import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
+import TestableMacros
 
 // Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
-#if canImport(TestableMacroMacros)
-import TestableMacroMacros
-
 let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
+    "Testable": TestableMacro.self,
 ]
-#endif
-
-final class TestableMacroTests: XCTestCase {
-    func testMacro() throws {
-        #if canImport(TestableMacroMacros)
-        assertMacroExpansion(
-            """
-            #stringify(a + b)
-            """,
-            expandedSource: """
-            (a + b, "a + b")
-            """,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
+final class MacroTests: XCTestCase {
+    
+    func testTestableMacro() throws {
+        let source = """
+class MyClass {
+    private var someVariable: Int = 0
+    private var someVariableGetOnly: Int { 0 }
+    private var somePropertyGetOnlyImplicit: String {
+        ""
     }
+    private var somePropertyGetOnlyExplicit: String {
+        get { "" }
+    }
+    private var somePropertyGetAndSetExplicit: String {
+        get { "" }
+        set { }
+    }
+    private func someFunction2() {
+    }
+    private func someFunction(param1: String, param2: Int) -> (() -> Void)? {
+        nil
+    }
+    private func anotherFunction2(_ param: Double, param2: Int) -> [String: String] {
+        [:]
+    }
+    fileprivate func anotherFunction(in param: Double) -> Double? {
+        nil
+    }
+}
+"""
+        
+        let expectedOutput = """
 
-    func testMacroWithStringLiteral() throws {
-        #if canImport(TestableMacroMacros)
-        assertMacroExpansion(
-            #"""
-            #stringify("Hello, \(name)")
-            """#,
-            expandedSource: #"""
-            ("Hello, \(name)", #""Hello, \(name)""#)
-            """#,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
+extension MyClass {
+    #if DEBUG
+    var testHooks: TestHooks {
+        return TestHooks(target: self)
+    }
+    struct TestHooks {
+        private var target: MyClass
+        fileprivate init(target: MyClass) {
+            self.target = target
+        }
+        var someVariable: Int {
+            get {
+                return target.someVariable
+            }
+            set {
+                target.someVariable = newValue
+            }
+        }
+        var someVariableGetOnly: Int {
+            get {
+                return target.someVariableGetOnly
+            }
+        }
+        var somePropertyGetOnlyImplicit: String {
+            get {
+                return target.somePropertyGetOnlyImplicit
+            }
+        }
+        var somePropertyGetOnlyExplicit: String {
+            get {
+                return target.somePropertyGetOnlyExplicit
+            }
+        }
+        var somePropertyGetAndSetExplicit: String {
+            get {
+                return target.somePropertyGetAndSetExplicit
+            }
+            set {
+                target.somePropertyGetAndSetExplicit = newValue
+            }
+        }
+        func someFunction2() -> Void {
+            return target.someFunction2()
+        }
+        func someFunction(param1: String, param2: Int) -> (() -> Void)? {
+            return target.someFunction(param1: param1, param2: param2)
+        }
+        func anotherFunction2(_ param: Double, param2: Int) -> [String: String] {
+            return target.anotherFunction2(_: param, param2: param2)
+        }
+        func anotherFunction(in param: Double) -> Double? {
+            return target.anotherFunction(in: param)
+        }
+    }
+    #endif
+}
+"""
+        assertMacroExpansion(["@Testable", source].joined(separator: "\n"),
+                             expandedSource: [source, expectedOutput].joined(separator: "\n"),
+                             macros: testMacros,
+                             indentationWidth: .spaces(4))
     }
 }
